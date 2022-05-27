9488a01b15577e1720e723485a3807c26b0c5abc
Eduardo Zerecero en Mac <eduardo@zerecero.com>
2022-05-26 19:52:38 -0500

    Primer commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Query } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey: string = 'czbBX7XoVkJx7a8eKU0MSYcdKbLXn0Fs';
+  private _historial: string[] = [];
+
+  //TODO: Cambiar any por sy tipo correspondiente
+  public resultados: Gif[] = [];
+
+  get historial() {
+    
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient) {
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || []
+
+    /* Esta es una manera de hacerlo igual a la de arriba
+    if(localStorage.getItem('historial')){
+      this._historial = JSON.parse(localStorage.getItem('historial')! )
+    }*/
+
+  }
+
+  buscarGifs(query: string ){
+
+    query = query.trim().toLowerCase();
+
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial))
+
+    }
+
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=czbBX7XoVkJx7a8eKU0MSYcdKbLXn0Fs&q=${query}&limit=10`)
+    .subscribe( (resp) => {
+      console.log(resp.data);
+      this.resultados = resp.data;
+      
+    })
+    
+  }
+  
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: [
+  ]
+})
+export class SidebarComponent implements OnInit {
+
+  get historial(){
+    return this.gifService.historial;
+  }
+
+  constructor(private gifService: GifsService) { }
+
+  ngOnInit(): void {
+  }
+
+}
